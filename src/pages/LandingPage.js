c9bce6da17b199837a51c2a7dfb85b4b6c79c289
mraf19 <raflimr1902@gmail.com>
2021-12-10 23:14:19 +0700

    Create Part Header and put it to landing page

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import Header from "parts/Header";
+import React from "react";
+
+const LandingPage = (props) => {
+	return (
+		<>
+			<Header {...props} />
+		</>
+	);
+};
+
+export default LandingPage;
---
+++ src/parts/TextIcon.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "element/Button";
+
+const TextIcon = () => {
+	return (
+		<Button className="brand-icon-text" type="link" href="/">
+			Stay<span className="text-gray-900">cation.</span>
+		</Button>
+	);
+};
+
+export default TextIcon;
